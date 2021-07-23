d537997ae1728dfec81d763a5527608248a92a4a
Lucky Bunny <luckybunny@air-lucky-bunny.lan>
2021-07-23 20:52:19 +0700

    add filter table

--- demo/urls.py
+++ demo/urls.py
@@ -6,6 +6,7 @@ app_name = "demo"
 urlpatterns = [
     path('auth/login/', auth_views.LoginView.as_view(template_name='authticate/login.html'), name='login'),
     path('auth/logout', auth_views.LogoutView.as_view(template_name='authticate/logout.html'), name='logout'),
+    path('search', views.search, name='search'),
     path('<str:f>', views.view, name='view'),
     path('city/<str:f>', views.city, name='city'),
     path('district/<str:f>', views.district, name='district'),
@@ -13,7 +14,7 @@ urlpatterns = [
     #url(r'^view/(?P<f>.+)$', views.view, name='view'),
     path('uploadfile', views.fileUploaderView, name='uploadfile'),
     url(r'^savedb/(?P<filename>.+)$', views.savedb, name='savedb'),
-    path('search', views.search, name='search'),
+
     path('detail/<str:bn>', views.detail, name='detail'),
     path('', views.index, name='index'),
     path('updatestatusF0/<int:id>', views.updateStatusF0, name='updateStatusF0'),
